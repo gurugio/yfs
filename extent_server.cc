c435bd94e2cc11824467ac4eba7236a77412f696
Gioh Kim <gurugio@hanmail.net>
2017-12-03 10:16:34 +0100

    struct yfsfile and put() method
    implemented put() method and struct yfsfile

--- extent_server.cc
+++ extent_server.cc
@@ -8,13 +8,34 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-extent_server::extent_server() {}
+extent_server::extent_server()
+{
+	file_map = new std::map<extent_protocol::extentid_t,
+				struct yfsfile *>;
+	pthread_mutex_init(&attr_lock, NULL);
+}
 
 
 int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
-  // You fill this in for Lab 2.
-  return extent_protocol::IOERR;
+	struct yfsfile *f;
+	std::map<extent_protocol::extentid_t, struct yfsfile *>::iterator it;
+
+	// You fill this in for Lab 2.
+	pthread_mutex_lock(&attr_lock);
+
+	it = file_map->find(id);
+	if (it == file_map->end()) {
+		f = new yfsfile();
+		file_map->insert(std::make_pair(id, f));
+	}
+
+	it = file_map->find(id);
+	f = it->second;
+	f->file_name = buf;
+
+	pthread_mutex_unlock(&attr_lock);
+	return extent_protocol::OK;
 }
 
 int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
--- extent_server.h
+++ extent_server.h
@@ -8,14 +8,28 @@
 #include "extent_protocol.h"
 
 class extent_server {
+protected:
+	struct yfsfile
+	{
+		yfsfile () {
+			file_attr.atime = 0;
+			file_attr.mtime = 0;
+			file_attr.ctime = 0;
+			file_attr.size = 0;
+		}
+		std::string file_name;
+		extent_protocol::attr file_attr;
+	};
 
- public:
-  extent_server();
+	std::map<extent_protocol::extentid_t, struct yfsfile *> *file_map;
+	pthread_mutex_t attr_lock;
 
-  int put(extent_protocol::extentid_t id, std::string, int &);
-  int get(extent_protocol::extentid_t id, std::string &);
-  int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
-  int remove(extent_protocol::extentid_t id, int &);
+public:
+	extent_server();
+	int put(extent_protocol::extentid_t id, std::string, int &);
+	int get(extent_protocol::extentid_t id, std::string &);
+	int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
+	int remove(extent_protocol::extentid_t id, int &);
 };
 
 #endif 
