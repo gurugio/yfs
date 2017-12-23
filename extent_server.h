8cd3edc5d30f04aaffd3b71083a540d7636ccdff
Gioh Kim <gurugio@hanmail.net>
2017-12-23 02:01:20 +0100

    implement extent-server
    yfsfile consists of attribute, buffer and name.
    Extent-server does not distinguish file and dir.
    Extent-server only manages a mapping between id and yfsfile object.

--- extent_server.h
+++ extent_server.h
@@ -8,21 +8,33 @@
 #include "extent_protocol.h"
 
 class extent_server {
+	struct yfsfile
+	{
+		yfsfile () {
+			file_attr.atime = 0;
+			file_attr.mtime = 0;
+			file_attr.ctime = 0;
+			file_attr.size = 0;
+		}
+		std::string file_name;
+		extent_protocol::attr file_attr;
+		std::string file_buf;
+	};
 
- public:
-  extent_server();
+	pthread_mutex_t attr_lock;
 
-  int put(extent_protocol::extentid_t id, std::string, int &);
-  int get(extent_protocol::extentid_t id, std::string &);
-  int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
-  int remove(extent_protocol::extentid_t id, int &);
+	// extent_server does not identify which is file or dir.
+	// Everything is file and has unique id and buffer.
+	// Data in the buffer does not matter.
+	std::map<extent_protocol::extentid_t, struct yfsfile *> *file_map;
+
+public:
+	extent_server();
+
+	int put(extent_protocol::extentid_t id, std::string, int &);
+	int get(extent_protocol::extentid_t id, std::string &);
+	int getattr(extent_protocol::extentid_t id, extent_protocol::attr &);
+	int remove(extent_protocol::extentid_t id, int &);
 };
 
-#endif 
-
-
-
-
-
-
-
+#endif
